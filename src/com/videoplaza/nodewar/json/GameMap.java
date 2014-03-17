58ea75bcffc4da12ee98118b7fd92537c477b16b
jakob <jakob@videoplaza.com>
2014-03-17 14:39:58 +0100

    Add map parser.

---
+++ src/com/videoplaza/nodewar/json/GameMap.java
@@ -0,0 +1,8 @@
+package com.videoplaza.nodewar.json;
+
+import java.util.List;
+
+public class GameMap {
+   public String src;
+   public List<Region> regions;
+}
---
+++ src/com/videoplaza/nodewar/json/Region.java
@@ -0,0 +1,11 @@
+package com.videoplaza.nodewar.json;
+
+import java.util.List;
+
+public class Region {
+   public int id;
+   public int x;
+   public int y;
+   public String name;
+   public List<Integer> neighbours;
+}
